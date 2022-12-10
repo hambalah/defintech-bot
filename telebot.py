339b98bad81085caa1c8ceb309cb06115ccb0f52
hambalah <kaydon.lim.2021@scis.smu.edu.sg>
2022-12-10 15:16:36 +0800

    add: kaydon's api key

--- telebot.py
+++ telebot.py
@@ -2,7 +2,9 @@ from telegram.ext import *
 from requests import *
 from telegram import *
 
-updater = Updater(token='5668531051:AAEeX4OWwO1sOPvIYMI-2nUyhTcz_UWQVH4',use_context=True)
+# updater = Updater(token='5668531051:AAEeX4OWwO1sOPvIYMI-2nUyhTcz_UWQVH4',use_context=True) #shawn's bot
+updater = Updater(token='5860916892:AAF8lhYm-CZiNigpGxbJehVAckG6w2ekHhk',use_context=True) #kaydon's bot
+
 dp = updater.dispatcher
 
 #telehamdle linked with account details
