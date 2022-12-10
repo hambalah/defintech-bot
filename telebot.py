cad68bd5cb0fda5d695d74311bb18599b7f145b0
nmywrld <111645407+nmywrld@users.noreply.github.com>
2022-12-10 12:58:39 +0800

    update

--- telebot.py
+++ telebot.py
@@ -2,12 +2,8 @@ from telegram.ext import *
 from requests import *
 from telegram import *
 
-
-
-INPUT = None
-
 # updater = Updater(token='5668531051:AAEeX4OWwO1sOPvIYMI-2nUyhTcz_UWQVH4',use_context=True)
-updater = Updater(token='5828726712:AAFx7vMV1rv3oiqJFyEhx76HpY2bqU_iVXI',use_context=True)
+updater = Updater(token='5828726712:AAH2mCRI9FKiQmZBgM_SEmHeQhMl23kEK88',use_context=True)
 
 # bot = TeleBot("5828726712:AAFx7vMV1rv3oiqJFyEhx76HpY2bqU_iVXI")
 dp = updater.dispatcher
@@ -22,85 +18,75 @@ database = {'shawn':{'bank':'posb', 'currency':'sgd', 'account':'12345', 'balanc
 current_account = ''
 
 
+# used for transfer processx
+receiverstate, trfamtstate = range(2)
+
+
+
+
+
 def login(update, context):
     update.message.reply_text('(url link to finverse)')
 
-def transfer_process (update, context):
-    # await update.message.reply_text("What is the Recpient's Telegram Handle?", reply_markup=ReplyKeyboardRemove)
-    # receiverTeleId = update.message.text
-    # await update.message.reply_text("What is the Transfer Amount?", reply_markup=ReplyKeyboardRemove)
-    # transferAmount = update.message.text
-    
-    receiverTeleId = None
-    while receiverTeleId == None:
-        update.message.reply_text("What is the Recpient's Telegram Handle?")
-        receiverTeleId = update.message.text
-    
-    transferAmount = None
-    while transferAmount == None:
-        update.message.reply_text("What is the Transfer Amount?")
-        transferAmount = update.message.text
 
-    # def getname (update, context):
-    #     msg = context.bot.send_message(chat_id=update.effective_chat.id, text= "What is the Recpient's Telegram Handle?")
-    #     receiverTeleId = update.message.text
-    #     bot.register_next_step_handler(msg, getamt)
-    
-    # def getamt (update, context):
-    #     context.bot.send_message(chat_id=update.effective_chat.id, text= "What is the Transfer Amount?")
-    #     transferAmount = update.message.text
+# transfer process
+def transfer_process(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Please Input Receivers' Telegram handle")
+    return receiverstate
 
-    # if receiverTeleId == None:
-    #     getname(update, context)
+def transfer_process_name (update, context):
+    context.user_data["receiverTeleId"] = update.message.text
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Please Input Transfer Amount")
+    return trfamtstate
 
-    print(receiverTeleId, transferAmount)
+def transfer_process_amt (update, context):
+    context.user_data["transferAmount"] = update.message.text
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'You want to send ${context.user_data["transferAmount"]} to @{context.user_data["receiverTeleId"]}')
 
-    # update.message.text == None
-
-    # receiverTeleId = update.message.text
-    # if receiverTeleId == None:
-    #     update.message.reply_text(f"Please Input Receivers' handle")
-    #     receiverTeleId = update.message.text
-
-    # update.message.reply_text(f"Please Input Transfer Amount")
-    # # updater.idle()
-    # # transferAmount = float(update.message.reply_text(f'please input Transfer amount'))
+    print(context.user_data)
+    return ConversationHandler.END
     
 
 def handle_message(update,context):
-    # text = str(update.message.text).lower()
-    # response = ''
-    # if 'hello' in text:
-    #     response = 'Hellooo!'
-    #     update.message.reply_text(f"{response} {update['message']['chat']['first_name']}.")
-    # if 'how are you' in text:
-    #     response = 'GRAAAAPEEEE!'
-    #     update.message.reply_text(f"I'm {response}, {update['message']['chat']['first_name']}.")
-    #     update.message.reply_text(f"how are you {update.message.chat.first_name}.")
-    # #update.message.chat.first_name == update['message']['chat']['first_name']
+    global transferFlag
+    global receiverTeleId
+    global transferAmount
+
     if 'Account Balance' in update.message.text:
         update.message.reply_text(f'{update.message.chat.username}, your account balance is {database[update.message.chat.username]["balance"]}')
     if 'Funds Transfer' in update.message.text:
-        transfer_process(update, context)
+        transferFlag = True 
     if 'Account Details' in update.message.text:
         update.message.reply_text(f'name: {update.message.chat.first_name}, account balance: XXX')
 
 
+
 def startCommands(update: Update, context:CallbackContext):
-    buttons = [[KeyboardButton('Account Balance')], [KeyboardButton('Funds Transfer')], [KeyboardButton('Change Bank Account')]]
+    buttons = [[KeyboardButton('Account Balance')], [KeyboardButton('/Transfer')], [KeyboardButton('Change Bank Account')]]
     print(update)
-    context.bot.send_message(chat_id=update.effective_chat.id, text='WELCOME!',reply_markup = ReplyKeyboardMarkup(buttons))
+    context.bot.send_message(chat_id=update.effective_chat.id, text='WELCOME!',reply_markup = ReplyKeyboardMarkup(buttons, one_time_keyboard=True))
+
+
+transaction_process_conv = ConversationHandler(
+    entry_points=[CommandHandler(f'Transfer', transfer_process)],
+    states={
+        receiverstate : [MessageHandler(Filters.text, callback=transfer_process_name)],
+        trfamtstate : [MessageHandler(filters= Filters.regex('[0-9]'), callback=transfer_process_amt)]
+    },
+    fallbacks=[CommandHandler('start', startCommands)]
+)
+
+
+dp.add_handler(CommandHandler('start', startCommands))
+dp.add_handler(transaction_process_conv)
+dp.add_handler(MessageHandler(Filters.text, handle_message))
+dp.add_handler(CommandHandler('login', login))
+
+updater.start_polling()
 
 
 # bot.enable_save_next_step_handlers(delay=2)
 # bot.load_next_step_handlers()
-
-dp.add_handler(CommandHandler('start', startCommands))
-dp.add_handler(MessageHandler(Filters.text, handle_message))
-dp.add_handler(CommandHandler('login', login))
-
-updater.start_polling(1.0)
-updater.idle
 
 # if __name__ == '__main__':
 #     Token = '5668531051:AAEeX4OWwO1sOPvIYMI-2nUyhTcz_UWQVH4'
